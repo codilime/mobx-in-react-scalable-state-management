2260333467e7b05bc7af818d3c976288c5f16f10
Piotr Gejgalis <piotr.gejgalis@codilime.com>
2021-10-12 10:12:27 +0200

    Extract setVariables to separate action

--- src/app/_common/graphql/query-result.ts
+++ src/app/_common/graphql/query-result.ts
@@ -3,11 +3,23 @@ import { ApolloClient } from 'apollo-client-preset';
 import { WatchQueryOptions } from 'apollo-client/core/watchQueryOptions';
 
 export class QueryResult<RESULT, VARIABLES> {
-  private subscription: ReturnType<typeof apolloQueryToMobxObservable>['subscription'] | undefined;
+  private subscription?: ReturnType<typeof apolloQueryToMobxObservable>['subscription'];
+
+  constructor(
+    private client: ApolloClient<{}>,
+    private queryOptions: Omit<WatchQueryOptions<VARIABLES>, 'variables'>,
+    private variables?: VARIABLES,
+  ) {
+    makeAutoObservable(this, {
+      // @ts-ignore
+      client: false,
+      queryOptions: false,
+    });
+  }
 
   private get result() {
     this.subscription?.unsubscribe();
-    const { result, subscription } = apolloQueryToMobxObservable(this.client, this.queryOptions);
+    const { result, subscription } = apolloQueryToMobxObservable(this.client, this.queryOptions, this.variables);
     this.subscription = subscription;
     return result;
   }
@@ -24,12 +36,8 @@ export class QueryResult<RESULT, VARIABLES> {
     return this.result.data;
   }
 
-  constructor(private client: ApolloClient<{}>, private queryOptions: WatchQueryOptions<VARIABLES>) {
-    makeAutoObservable(this, {
-      // @ts-ignore
-      client: false,
-      query: false,
-    });
+  setVariables(variables: VARIABLES | undefined) {
+    this.variables = variables;
   }
 
   dispose() {
@@ -37,8 +45,12 @@ export class QueryResult<RESULT, VARIABLES> {
   }
 }
 
-function apolloQueryToMobxObservable<VARIABLES>(client: ApolloClient<{}>, queryOptions: WatchQueryOptions<VARIABLES>) {
-  const query = client.watchQuery(queryOptions);
+function apolloQueryToMobxObservable<VARIABLES>(
+  client: ApolloClient<{}>,
+  queryOptions: Omit<WatchQueryOptions<VARIABLES>, 'variables'>,
+  variables: VARIABLES | undefined,
+) {
+  const query = client.watchQuery({ ...queryOptions, variables });
   const result = observable(query.currentResult());
 
   const subscription = query.subscribe({
