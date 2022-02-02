58249dc19048b09afc0c59c4be04abf4e3ccb8ce
Piotr Gejgalis <piotr.gejgalis@codilime.com>
2022-02-02 15:22:54 +0100

    Removed not necessary <App/> component

--- src/app/app.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AppModule } from './app-module';
-
-export const App = () => {
-  return <AppModule />;
-};
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { App } from './app/app';
+import { AppModule } from '@/app/app-module';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppModule />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
