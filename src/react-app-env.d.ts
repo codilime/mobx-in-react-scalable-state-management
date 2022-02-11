83b4eb92df1ff5ec03e6129e972025ab154dbfea
Piotr Gejgalis <piotr.gejgalis@codilime.com>
2022-02-11 15:42:19 +0100

    Fix Webstorm

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="react-scripts" />
-
-import { Class } from 'type-fest';
-import * as reactIoc from 'react-ioc';
-
-declare module 'react-ioc' {
-  function inject<T>(target: any, clazz: Class<T>): T; // Fix for WebStorm bug
-
-  export default {
-    ...reactIoc,
-    inject,
-  };
-}
