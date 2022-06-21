f3f92636540c6aecffa918e29e052f804042c6a3
Piotr Gejgalis <piotr.gejgalis@codilime.com>
2022-06-21 08:14:30 +0200

    Delay on REST server

--- backend/server/server-rest.mjs
+++ backend/server/server-rest.mjs
@@ -1,45 +1,81 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { serverState } from './server-state.mjs';
+import _ from 'lodash';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const expressPort = 4001;
 
+const RESPONSE_DELAY = 1000;
+
 app.get('/api/users', (req, res) => {
-  res.json(serverState.users);
+  setTimeout(() => res.json(serverState.users), RESPONSE_DELAY);
 });
 
 app.get('/api/users/:id/details', (req, res) => {
-  res.json({
-    auditTrail: [
-      {
-        id: '333',
-        name: 'Sign Out',
-        details: 'Host: 1.2.3.4, Linux, Chrome',
-        createdAt: 'Jan 7, 2022 16:23',
-      },
-      {
-        id: '222',
-        name: 'Password change',
-        details: 'Host: 1.2.3.4, Linux, Chrome',
-        createdAt: 'Jan 7, 2022 14:08',
-      },
-      {
-        id: '111',
-        name: 'Sign In',
-        details: 'Host: 1.2.3.4, Linux, Chrome',
-        createdAt: 'Jan 7, 2022 12:34',
-      },
-    ],
-  });
+  setTimeout(() => {
+    if (req.params.id === '1') {
+      res.json({
+        auditTrail: _.shuffle([
+          {
+            id: '333',
+            name: 'Sign Out',
+            details: 'Host: 1.2.3.4, Linux, Chrome',
+            createdAt: 'Jan 7, 2022 16:23',
+          },
+          {
+            id: '222',
+            name: 'Password change',
+            details: 'Host: 1.2.3.4, Linux, Chrome',
+            createdAt: 'Jan 7, 2022 14:08',
+          },
+          {
+            id: '111',
+            name: 'Sign In',
+            details: 'Host: 1.2.3.4, Linux, Chrome',
+            createdAt: 'Jan 7, 2022 12:34',
+          },
+        ]),
+      });
+    } else {
+      res.json({
+        auditTrail: _.shuffle([
+          {
+            id: '666',
+            name: 'Sign Out',
+            details: 'Host: 10.0.0.123, Windows, Firefox',
+            createdAt: 'Feb 18, 2022 10:23',
+          },
+          {
+            id: '777',
+            name: 'Password change',
+            details: 'Host: 10.0.0.123, Windows, Firefox',
+            createdAt: 'Feb 17, 2022 07:08',
+          },
+          {
+            id: '888',
+            name: 'Sign In',
+            details: 'Host: 10.0.0.123, Windows, Firefox',
+            createdAt: 'Jan 13, 2022 12:34',
+          },
+          {
+            id: '999',
+            name: 'Password change',
+            details: 'Host: 10.0.0.123, Windows, Firefox',
+            createdAt: 'Jan 11, 2022 12:34',
+          },
+        ]),
+      });
+    }
+  }, RESPONSE_DELAY);
 });
 
 app.post('/api/users', (req, res) => {
   const newUser = { ...req.body, id: serverState.nextUserId() };
   serverState.users.push(newUser);
-  res.json(newUser);
+  setTimeout(() => res.json(newUser), RESPONSE_DELAY);
 });
 
 app.put('/api/users/:id', (req, res) => {
@@ -47,9 +83,9 @@ app.put('/api/users/:id', (req, res) => {
   const user = serverState.users.find((u) => u.id === userId);
   if (user) {
     Object.assign(user, req.body);
-    res.json(user);
+    setTimeout(() => res.json(user), RESPONSE_DELAY);
   } else {
-    res.sendStatus(404);
+    setTimeout(() => res.sendStatus(404), RESPONSE_DELAY);
   }
 });
 
@@ -58,7 +94,7 @@ app.delete('/api/users', (req, res) => {
   serverState.users = serverState.users.filter(
     (user) => !ids.includes(user.id.toString()),
   );
-  res.sendStatus(200);
+  setTimeout(() => res.sendStatus(200), RESPONSE_DELAY);
 });
 
 app.listen(expressPort, () => {
